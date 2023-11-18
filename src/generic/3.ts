0a49d4e93989b518c3b895be216e1313b81dd4af
Artur-2023 <123097639+arthyr-men@users.noreply.github.com>
2023-11-18 16:52:25 +0100

    1-2

--- src/generic/3.ts
+++ src/generic/3.ts
@@ -1,10 +1,12 @@
-/*
-  У вас є функція merge, яка поєднує два об'єкти. 
-  Використовуйте generics, щоб вказати, що ці об'єкти можуть бути будь-якого типу.
-*/
+// Визначення типу для об'екта, який може мати будь-які властивості//
+type AnyObject = Record<string, any>;
 
-function merge<T, U>(objA: T, objB: U): T & U {
+// Функція merge з використанням generics та типу для об'єкта
+function merge<T extends AnyObject, U extends AnyObject>(objA: T, objB: U): T & U {
   return Object.assign({}, objA, objB);
 }
 
-export { }
+// Приклад використання
+const mergedObject = merge({ name: 'John' }, { age: 25 });
+
+console.log(mergedObject);
